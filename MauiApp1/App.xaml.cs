c6c4acd69597a4a8afb52dd1c345d4a09f3b0d8b
JoshuaC09 <joshuacanapi09@gmail.com>
2024-06-07 20:20:32 +0800

    First commit

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MauiApp1.Services;
+using Microsoft.Maui.Controls;
+using System.Net.Http;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var httpClient = new HttpClient();
+            var httpClientService = new HttpClientService(httpClient);
+            MainPage = new NavigationPage(new MainPage(httpClientService));
+        }
+    }
+}
